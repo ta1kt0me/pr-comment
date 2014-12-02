998be016b95b5de6274746bd52b622f64495def4
ta1kt0me <p.wadachi@gmail.com>
2014-12-02 17:32:55 +0900

    implements `prc close`

--- lib/pr/comment/cli.rb
+++ lib/pr/comment/cli.rb
@@ -9,9 +9,26 @@ module PR
 
       desc "all ORG/REPO PR_NO", "print all pull request comments list to STDOUT."
       def all(repo, pr_no)
-
-
         sorted_comments = collected_comments(repo, pr_no).summarize_and_sort
+        print_comment(sorted_comments)
+      end
+
+      desc "close ORG/REPO PR_NO", "print close pull request comments list to STDOUT."
+      def close(repo, pr_no)
+        sorted_comments = collected_comments(repo, pr_no).exclude_close_comments!.summarize_and_sort
+        print_comment(sorted_comments)
+      end
+
+      private
+      def collected_comments(repo, pr_no)
+        client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+        comments = Pr::Comment::CollectedComments.new
+        comments.add_issue_comment!(client, repo, pr_no)
+        comments.add_pr_comment!(client, repo, pr_no)
+        comments
+      end
+
+      def print_comment(sorted_comments)
         sorted_comments.each_with_index do |elem, idx|
           elem.each_with_index do |item, idx|
             if item.review and idx == 0
@@ -29,20 +46,6 @@ module PR
           end
           say '|' if idx + 1 != sorted_comments.size
         end
-      end
-
-      desc "close ORG/REPO PR_NO", "print close pull request comments list to STDOUT."
-      def close(repo, pr_no)
-
-      end
-
-      private
-      def collected_comments(repo, pr_no)
-        client = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
-        comments = Pr::Comment::CollectedComments.new
-        comments.add_issue_comment!(client, repo, pr_no)
-        comments.add_pr_comment!(client, repo, pr_no)
-        comments
       end
     end
   end
